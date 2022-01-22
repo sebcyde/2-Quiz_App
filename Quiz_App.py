d2f64a56cfdc7f05bcd24a0a042478ec62d8e65c
Seb <sebcyde@hotmail.com>
2022-01-22 22:38:12 +0000

    added in more questions

--- Quiz_App.py
+++ Quiz_App.py
@@ -1,2 +1,33 @@
 print('hello world')
-playing = input('Do you want to play a game? ')
+playing = input('Do you want to play a game? Y/N ')
+if playing != 'y':
+    quit()
+
+print("Okay, let's play!")
+answer = input('What does CPU stand for? ')
+if answer == 'central processing unit':
+    print('Correct!')
+else: print("Incorrect!")
+
+print("Let's try another one!")
+answer2 = input('What is the best front end framework? ')
+if answer2 == 'react':
+    print('Correct!')
+else: print("Incorrect!")
+
+continueplaying = input("Should we try another one? Y/N ")
+if continueplaying != 'y':
+    quit()
+
+print("Okay, let's carry on!")
+answer3 = input(' ')
+if answer3 == '':
+    print('Correct!')
+else: print("Incorrect!")
+
+
+print("Let's try another one!")
+answer2 = input(' ')
+if answer2 == '':
+    print('Correct!')
+else: print("Incorrect!")
